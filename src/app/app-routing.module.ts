068f71a0c65d1c7c40aea4bd3507dab5c4001912
Nina Ruiz <nina.ruizandres@gmail.com>
2020-05-10 16:50:22 +0200

    Made register and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
+import {CreateCaracterComponent} from './pages/create-caracter-page/create-caracter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create-caracter', component: CreateCaracterComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: '', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
