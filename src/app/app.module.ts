6b0cead92361bcedbf9b85eb7d26c6f4703bcf29
Nina Ruiz <nina.ruizandres@gmail.com>
2020-05-10 16:35:12 +0200

    Create login, register and create caracter pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { CreateCaracterComponent } from './pages/create-caracter/create-caracter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    CreateCaracterComponent
   ],
   imports: [
     BrowserModule,
